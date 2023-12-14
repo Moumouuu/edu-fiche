cb938b02d90df8b350c3fe4020ec378baf80f63e
Moumouuu <robin.travail007@gmail.com>
2023-12-14 13:39:31 +0100

    Update Resend API key

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -2,7 +2,7 @@ import { EmailTemplate } from "@/components/email-template";
 import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   const { message, sender } = await req.json();
