c30ef46ede1d177ae711508c496d12cee5327c49
RobinWiilog <robin.pluviaux@wiilog.fr>
2024-01-05 16:24:24 +0100

    Generate PWA configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-const { Domain } = require('domain')
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ["lh3.googleusercontent.com","avatar.vercel.sh"]
-    }
-}
+  swcMinify: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "avatar.vercel.sh"],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
+};
 
-module.exports = nextConfig
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig);
