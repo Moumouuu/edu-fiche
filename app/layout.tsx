c78f11bc73b7ab6d7a0e05bd3a21eeb769af5423
Moumouuu <robin.travail007@gmail.com>
2024-05-19 12:28:17 +0200

    fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { authOptions } from "@/lib/auth";
 
 import AuthContext from "@/app/providers/auth-provider";
 import { ThemeProvider } from "@/app/providers/theme-provider";
-import Head from "next/head";
 
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
@@ -54,11 +53,10 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
   return (
     <>
-      <Head>
-        <GoogleAnalytics />
-      </Head>
       <html lang="en" suppressHydrationWarning>
         <body className={roboto.className}>
+          <GoogleAnalytics />
+
           <Toaster />
           <AuthContext session={session}>
             <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -5,10 +5,10 @@ const GoogleAnalytics = () => {
   return (
     <>
       <Script
-        defer
+        async
         src="http://umami-yck480o.5.75.226.51.sslip.io/script.js"
         data-website-id="6a44a36d-e1a7-44ce-99a4-f80d00620193"
-      ></Script>
+     />
     </>
   );
 };
